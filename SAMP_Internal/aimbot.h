691589cf96c599813a8b1f3f233efccf48e95bcd
marioHub555 <hoho74817@gmail.com>
2026-04-25 07:25:24 -0700

    Add files via upload

---
+++ SAMP_Internal/aimbot.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "gui.h" // هذا الملف موجود ومجرب في مشروعك
+#include <vector>
+
+namespace GlobalFriends {
+	extern std::vector<int> list;
+	extern bool ignoreSameColor;
+}
+
+namespace AIMBOT {
+	float getFov(Vector3 aimAngle);
+	Vector3 calcAngle(actor_info* target);
+	actor_info* getEnemy();
+	void aim();
+}
